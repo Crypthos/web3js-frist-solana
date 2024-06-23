4b7fbe07a4ae8dabe45727ce5a36e0736da2dc04
Dalvin Newland <newlank@outlook.com>
2024-06-23 20:06:30 +0200

    1. how to keypair

--- generate-keypair/generate-keypair.ts
+++ generate-keypair/generate-keypair.ts
@@ -2,7 +2,7 @@ import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 import { Keypair } from '@solana/web3.js';
 // generate a new keypair
 const keypair = Keypair.generate();
-// console.log(`✅ Generated keypair!`);
+console.log(`✅ Generated keypair!`);
 
 console.log(`The public key is: `, keypair.publicKey.toBase58());
 console.log(`The secret key is: `, keypair.secretKey);
