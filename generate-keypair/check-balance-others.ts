57718d379ef42303b562e3cf6e7285706d34aef2
Dalvin Newland <newlank@outlook.com>
2024-07-02 14:45:31 +0200

    add check-balance-others.ts - user address input

--- generate-keypair/check-balance-others.ts
+++ generate-keypair/check-balance-others.ts
@@ -1,29 +1,13 @@
-import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
-import {getHashedNameSync, getNameAccountKeySync, NameRegistryState} from "@bonfida/spl-name-service"
+import {clusterApiUrl, Connection, LAMPORTS_PER_SOL, PublicKey} from "@solana/web3.js";
 
-
-
-const suppliedPublicKeyOrDomain  = process.argv[2];
-if (!suppliedPublicKeyOrDomain ) {
-    throw new Error("Provide a public key or SNS domain to check the balance of!");
+const suppliedPublicKey = process.argv[2];
+if (!suppliedPublicKey) {
+    throw new Error("Provide a public key to check the balance of!");
 }
 
-const isValidBase58 = (str: string) => {
-    const base58Regex = /^[1-9A-HJ-NP-Za-km-z]+$/;
-    return base58Regex.test(str);
-}
+const connection = new Connection(clusterApiUrl("devnet"),"confirmed");
 
-// Function to resolve SNS domain to a public key
-const resolveDomain = async (domain: string): Promise<PublicKey> => {
-    const hashedName = await getHashedNameSync(domain);
-    const nameAccountKey = await getNameAccountKeySync(hashedName, new PublicKey("SNS111111111111111111111111111111111111111"));
-    const registry = await NameRegistryState.retrieve(connection, nameAccountKey);
-    return registry.owner;
-};
-
-const connection = new Connection("https://api.mainnet-beta.solana.com", "confirmed");
-
-const publicKey = new PublicKey(suppliedPublicKeyOrDomain );
+const publicKey = new PublicKey(suppliedPublicKey);
 
 const balanceInLamports = await connection.getBalance(publicKey);
 
