4e74d77d26b5a707d78efd9662d7ae33485b14dc
Dalvin Newland <newlank@outlook.com>
2024-07-02 19:09:28 +0200

    Challenge 1 Task 2 complete

--- generate-keypair/check-balance-others.ts
+++ generate-keypair/check-balance-others.ts
@@ -1,25 +1,53 @@
 import {clusterApiUrl, Connection, LAMPORTS_PER_SOL, PublicKey} from "@solana/web3.js";
+import {resolve} from "@bonfida/spl-name-service";
 
-const suppliedPublicKey = process.argv[2];
-let publicKey;
 
-if (!suppliedPublicKey) {
+const suppliedAddress = process.argv[2];
+if (!suppliedAddress) {
     throw new Error("Provide a public key to check the balance of!");
 }
 
-const connection = new Connection(clusterApiUrl("mainnet-beta"),"confirmed");
+const connection = new Connection(clusterApiUrl("mainnet-beta"), "bonfida");
 
-try {
-    publicKey = new PublicKey(suppliedPublicKey);
-} catch (error) {
-    throw new Error("❌ Invalid public key provided. Please check the address and try again.");
+async function getPublicKey(address: string) {
+    try {
+        //Try to resolve as domain name;
+        return await resolve(connection, address);
+    } catch (error) {
+        //If it fails, try to parse as public key;
+        try {
+            return new PublicKey(address);
+        } catch (publicKeyError) {
+            throw new Error("Invalid address or domain name provided. Please check the address and try again.");
+        }
+
+    }
 }
 
+async function getBalance(publicKey: PublicKey) {
+    const balanceInLamports = await connection.getBalance(publicKey);
+    const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+    return balanceInSOL;
+}
 
-const balanceInLamports = await connection.getBalance(publicKey);
+(async () => {
+    let publicKey;
+    try {
+        publicKey = await getPublicKey(suppliedAddress);
+    } catch (error) {
+        console.error(`❌ ${error.message}`);
+        process.exit(1);
 
-const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+    }
 
-console.log(
-    `✅ Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
-);
+    try {
+        const balanceInSOL = await getBalance(publicKey);
+        console.log(
+            `✅ Finished! The balance for the wallet at address ${publicKey.toBase58()} is ${balanceInSOL}!`
+        );
+    } catch (error) {
+        console.error(`❌ Failed to get balance for ${suppliedAddress} (${publicKey.toBase58()}).`);
+    }
+})();
+
+
